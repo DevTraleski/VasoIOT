a43d66da89ecd17bb3e2f76c13958815015c7934
DevTraleski <dev.traleski@gmail.com>
2018-11-12 22:35:05 +0000

    Changes to be committed: 	modified:   vasoiot.py

--- vasoiot.py
+++ vasoiot.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import time
 
 user = '6c6d5650-e6a4-11e8-810f-075d38a26cc9'
 password = '713db477c813147b9b07cc2056b70d787e6aa709'
@@ -9,3 +10,9 @@ port = 1883
 client = mqtt.Client(client_id)
 client.username_pw_set(user, password)
 client.connect(server, port)
+
+while(True):
+	client.publish('v1/6c6d5650-e6a4-11e8-810f-075d38a26cc9/things/68433c30-e6c6-11e8-810f-075d38a26cc9/data/0', 100)
+	time.sleep(5)
+	client.publish('v1/6c6d5650-e6a4-11e8-810f-075d38a26cc9/things/68433c30-e6c6-11e8-810f-075d38a26cc9/data/0', 0)
+	time.sleep(5)
